8f3582b2d06929f6e4473de3ddcec20996a5c27a
Takahiro Ikeda <ikeadless@gmail.com>
2015-07-12 09:51:45 +0900

    承認テーブル追加とアソシエーション

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :approve
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711173826) do
+ActiveRecord::Schema.define(version: 20150712004748) do
+
+  create_table "approves", force: :cascade do |t|
+    t.integer  "article_id", limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
